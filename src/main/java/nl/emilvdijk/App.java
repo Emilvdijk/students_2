718d947d5a8921f0eceec820dedc4e1dc2ccc2c5
emil <emilvdijk@gmail.com>
2024-04-04 22:17:40 +0200

    Initial commit

---
+++ src/main/java/nl/emilvdijk/App.java
@@ -0,0 +1,13 @@
+package nl.emilvdijk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
